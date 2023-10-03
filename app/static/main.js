d5cd2b91afad787c52e2c4bc9a6838c89f19a8f8
Nikita Manchuk <38407912+klorman@users.noreply.github.com>
2023-10-03 23:57:32 +0300

    Add request page. Refs #4

---
+++ app/static/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.select2-multiple').select2({
+      theme: 'bootstrap-5',
+      closeOnSelect: false,
+      width: 'resolve',
+      tags: false,
+      allowHtml: true,
+			allowClear: true
+    });
+})
