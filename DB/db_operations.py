01510cd77b0df0e3b12fc55173fed8ec3aca1aaa
Wesdcv <70857676+Wesdcv@users.noreply.github.com>
2023-10-11 15:00:08 +0300

    Update db_operations.py
    Corrected a few misspells

--- DB/db_operations.py
+++ DB/db_operations.py
@@ -14,7 +14,7 @@ def connect_to_database():
 
 #Adding a new doctor
 def add_doctor(doctorname, password, last_login): #TODO Add try/catch
-    query = "INSERT INTO doctors (doctorname, password, last_login) VALUES (%s, %s, %s)"
+    query = "INSERT INTO doctors (username, password, last_login) VALUES (%s, %s, %s)"
     values = (doctorname, password, last_login)
     cursor.execute(query, values)
     conn.commit()
@@ -70,7 +70,7 @@ def get_patient_id_by_name(patient_name):
 
 # Get doctor ID by doctor name
 def get_doctor_id_by_name(doctor_name):
-    query = "SELECT id FROM doctors WHERE doctorname = %s"
+    query = "SELECT id FROM doctors WHERE username = %s"
     cursor.execute(query, (doctor_name,))
     doctor_id = cursor.fetchone()
     return doctor_id
@@ -106,7 +106,7 @@ def get_available_patients_for_doctor(doctor_id):
 # Check if the provided doctor's password is correct
 def check_doctor_password(doctorname, password):
 
-    query = "SELECT password FROM doctors WHERE doctorname = %s"
+    query = "SELECT password FROM doctors WHERE username = %s"
 
     cursor.execute(query, (doctorname,))
     result = cursor.fetchone()
@@ -149,63 +149,3 @@ def add_request_diseases(request_id, disease_ids):
     except Exception as e:
         print("Error adding diseases to the request:", str(e))# Get a list of available patients for a doctor by doctor ID
 
-#Get a list of patients that are visible for the doctor
-def get_available_patients_for_doctor(doctor_id):
-
-    query = """
-    SELECT patients.id, patients.name
-    FROM patients
-    INNER JOIN doctor_patients ON patients.id = doctor_patients.patient_id
-    WHERE doctor_patients.doctor_id = %s
-    """
-
-    cursor.execute(query, (doctor_id,))
-    available_patients = cursor.fetchall()
-
-    return available_patients
-
-# Check if the provided doctor's password is correct
-def check_doctor_password(doctorname, password):
-
-    query = "SELECT password FROM doctors WHERE doctorname = %s"
-
-    cursor.execute(query, (doctorname,))
-    result = cursor.fetchone()
-
-    if result:
-        stored_password = result[0]
-        if stored_password == password:
-            return True
-    return False
-
-# Add symptoms to a request
-def add_request_symptoms(request_id, symptom_ids):
-
-    try:
-        insert_query = "INSERT INTO request_symptoms (request_id, symptom_id) VALUES (%s, %s)"
-
-        # Insert each symptom for the request
-        for symptom_id in symptom_ids:
-            cursor.execute(insert_query, (request_id, symptom_id))
-
-        conn.commit()
-        print("Symptoms added to the request successfully.")
-
-    except Exception as e:
-        print("Error adding symptoms to the request:", str(e))
-
-# Add diseases to a request
-def add_request_diseases(request_id, disease_ids):
-
-    try:
-        insert_query = "INSERT INTO request_diseases (request_id, disease_id) VALUES (%s, %s)"
-
-        # Insert each disease for the request
-        for disease_id in disease_ids:
-            cursor.execute(insert_query, (request_id, disease_id))
-
-        conn.commit()
-        print("Diseases added to the request successfully.")
-
-    except Exception as e:
-        print("Error adding diseases to the request:", str(e))
