0cc8633feb092b860e3523c3a61bed49f4de4be6
Wesdcv <70857676+Wesdcv@users.noreply.github.com>
2023-10-18 22:05:32 +0300

    Added retry decorator for db functions
    Main features: logs (DEBUG) into file, (WARNING) into stdout; if exception is raised, tries to execute function up to N times with a certain delay;

--- DB/database.py
+++ DB/database.py
@@ -2,6 +2,7 @@ import mysql.connector
 import csv
 from json import loads
 from hashlib import sha256
+from retry import retry
 
 class Database:
     def __init__(self):
@@ -15,18 +16,27 @@ class Database:
         self.execute_sql_script("./create_db_script.sql") #TODO добавить проверку перед выполнением, созданы ли все таблицы и связи
         self.fill_from_dataset("../datasets/Symptom-severity.csv", "symptoms")
         self.fill_from_dataset("../datasets/symptom_Description.csv", "diseases")
-    # Establish MySQL database
-    def execute_sql_script(self, script_file):
+    
+    @retry(total_tries=1)
+    def safe_commit(self):
         try:
-            with open(script_file, 'r') as file:
+            self.conn.commit()
+        except mysql.connector.Error as e:
+            self.conn.rollback()
+            msg = str('Function: safe_commit initiated rollback due to error during commit \n')
+            logger.warning(msg) 
+            raise
+
+    # Establish MySQL database
+    @retry()
+    def execute_sql_script(self, script_file):
+        with open(script_file, 'r') as file:
                 sql_script = file.read()
                 sql_commands = sql_script.split(';')
                 for command in sql_commands:
                     if command.strip():
                         self.cursor.execute(command)
-                self.conn.commit()
-        except Exception as e:
-            print(f"Error executing SQL script from file: {str(e)}") #TODO логировать ошибки, а не выводить на экран
+                self.safe_commit()
             
     def fill_from_dataset(self, filepath, tablename):
         data_set = set()
@@ -43,6 +53,7 @@ class Database:
                 print(f"Failed to insert values from dataset {tablename} into DB: {str(e)}")
         self.conn.commit()
 
+    @retry()
     def select_doctor_by_id(self, id):
         query = "SELECT id, username, name, password_hash, last_login, \
             is_blocked, image_path_location FROM doctors WHERE id = %s"
@@ -50,6 +61,7 @@ class Database:
         self.cursor.execute(query, values)
         return self.cursor.fetchone()
         
+    @retry()
     def select_doctor_by_username(self, username):
         query = "SELECT id, username, name, password_hash, last_login, \
             is_blocked, image_path_location FROM doctors WHERE username = %s"
@@ -57,16 +69,14 @@ class Database:
         self.cursor.execute(query, values)
         return self.cursor.fetchone() 
         
+    @retry()
     def insert_doctor_credentials(self, username, password_hash): #TODO использовать хэш пароля
-        try:
-            query = "INSERT INTO doctors (username, password_hash) VALUES (%s, %s)"
-            values = (username, password_hash)
-            self.cursor.execute(query, values)
-            self.conn.commit()
-        except Exception as e:
-            self.conn.rollback() #db rolls back by itself generally
-            print(f"Failed to insert doctor's credentials: {str(e)}")
-
+        query = "INSERT INTO doctors (username, password_hash) VALUES (%s, %s)"
+        values = (username, password_hash)
+        self.cursor.execute(query, values)
+        self.safe_commit()        
+            
+    @retry()
     def close(self):
         self.cursor.close()
         self.conn.close()
