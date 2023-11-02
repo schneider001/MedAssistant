62e8bf40b2dfcd2615129fec622e2e366db2b8c9
Wesdcv <70857676+Wesdcv@users.noreply.github.com>
2023-11-02 20:31:31 +0300

    Update database.py
    now initing utility is separate

--- app/database.py
+++ app/database.py
@@ -17,41 +17,7 @@ class Database:
             self.logger.warning(f'Database connection failed to iniate: {e}')
             raise
         self.conn.autocommit = False
-        self.cursor = self.conn.cursor()
-        self.execute_sql_script("../db_init/create_db_script.sql") #TODO добавить проверку перед выполнением, созданы ли все таблицы и связи
-        self.fill_from_dataset("../datasets/Symptom-severity.csv", "symptoms")
-        self.fill_from_dataset("../datasets/symptom_Description.csv", "diseases")
-        
-    def execute_sql_script(self, script_file):
-        with open(script_file, 'r') as file:
-                sql_script = file.read()
-                sql_commands = sql_script.split(';')
-                try:
-                    for command in sql_commands:
-                        if command.strip():
-                            self.cursor.execute(command)
-                    self.conn.commit()
-                    self.logger.info('Database created successfully')
-                except mysql.connector.Error as e:
-                    self.logger.warning(f'Database failed to create: {e}')
-                    self.conn.rollback()
-                    raise
-
-    def fill_from_dataset (self, filepath, tablename):
-        data_set = set()
-        with open(filepath, 'r') as csv_file:
-            reader = csv.reader(csv_file)
-            for row in reader:
-                data_set.add(row[0])
-        for entry in data_set:
-            query = f"INSERT IGNORE INTO {tablename} (name) VALUES (%s)"
-            values = (entry,)
-            try:
-                self.cursor.execute(query, values)
-            except Exception as e:
-                print(f"Failed to insert values from dataset {tablename} into DB: {str(e)}")
-        self.conn.commit()
-    
+        self.cursor = self.conn.cursor()    
     
     #example 1: execute_select("SELECT * FROM users where id = %s", id) -> [(id, name, pass)]
     #example 2: execute_select("SELECT id, name FROM users) -> [(id1, name1), (id2, name2), ...]
