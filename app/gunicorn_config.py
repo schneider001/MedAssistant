bd136afc7788a0bce30eca9790a1aadff9a514f5
schneider001 <platov.va@phystech.edu>
2023-12-18 00:12:02 +0300

    Delete unused configs

--- app/gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-# Enable logging to console
-errorlog = '-'  # Log to stdout
-accesslog = '-'
-loglevel = 'debug'  # Set log level to error
-
-# Set eventlet debug level to debug
-import eventlet
-eventlet.hubs.use_hub()
